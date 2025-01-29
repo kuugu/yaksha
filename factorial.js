37326ddf3d93afa1067f5ba524045821a17638b9
kuugu <kuugu.work@gmail.com>
2025-01-29 11:22:41 +0530

    first two files

--- factorial.js
+++
@@ -1,9 +0,0 @@
-function factorial(x) {
-    if (x == 0) return 1; 
-    else if (x == 1) return 1; 
-    else return x * factorial(x-1); 
-}
-
-alert('running factorial function')
-factorial(24);
-alert('done.')
