836afb472fae3f99f9af0aaf0fc9474c6f26da39
kuugu <kuugu.work@gmail.com>
2025-02-07 18:14:39 +0530

    table done

--- populate_data.js
+++ populate_data.js
@@ -1,2 +1,55 @@
-console.log('hello')
+console.log('hello1')
 
+async function getDataFromJson() {
+
+    try {
+        const response = await fetch('table.json')
+
+        if (!response.ok) {
+            throw new Error(`${response.status}`)
+        }
+
+        const json = await response.json(); 
+
+        return json; 
+    } catch (error) {
+        console.log(error)
+
+        return null; 
+    }
+}
+
+let table_data = getDataFromJson(); 
+console.log('hello2')
+console.log(table_data)
+console.log('hello2')
+
+table_data.then(
+    (value) => {
+        const doc = document.getElementById("table")
+        let header = document.createElement('tr') 
+
+        Object.keys(value[0]).forEach(element => {
+            let th = document.createElement('th') 
+            th.textContent = element; 
+            header.appendChild(th); 
+        }) 
+
+        document.getElementById("table").appendChild(header); 
+
+        value.forEach(element => {
+            let tr = document.createElement('tr'); 
+
+            // console.log(element);
+
+            for (let elem in element) {
+                let td = document.createElement('td')
+                td.textContent = element[elem]; 
+                tr.appendChild(td);
+            }
+
+            document.getElementById("table").appendChild(tr) 
+        });
+    }
+)
+console.log('hello2')
